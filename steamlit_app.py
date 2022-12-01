b61282071e366165a0e002808ca81ea0abcb31e5
Allen Courtway <118224353+ac11gitHubSF@users.noreply.github.com>
2022-12-01 09:11:46 -0600

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -42,3 +42,7 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+my_cur.execute("select * from fruit_load_list")
+streamlit.text("The Fruit Load List Contains:")
+streamlit.text(my_data_row)
