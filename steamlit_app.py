04e4034ba8dc8a2dbb5460becd97509d5ce182fb
Allen Courtway <118224353+ac11gitHubSF@users.noreply.github.com>
2022-12-01 10:34:15 -0600

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -88,6 +88,11 @@ if streamlit.button('Get Fruit Load List'):
   #      my_cur.execute("insert into fruit_load_list values ('from streamlit')")
    #     return "Thanks for adding " + new_fruit
     
+#challenge lab fix code:
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('new_fruit')")
+        return "Thanks for adding " + new_fruit
     
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
@@ -95,11 +100,7 @@ if streamlit.button('Add a fruit to the list'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
     
-#challenge lab fix code:
-def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('new_fruit')")
-        return "Thanks for adding " + new_fruit
+
     
 #streamlit.stop()    
 #my_cur.execute("select * from fruit_load_list")
